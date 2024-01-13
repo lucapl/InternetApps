ec552ec49da02648b4e00f3b09afafe55a1444c0
lucapl <lukasz.andryszewski@outlook.com>
2024-01-13 20:35:13 +0100

    Asp.NET tutorial

---
+++ Lab7-ASP_DOT_NET/asp_tutorial/Name.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace asp_tutorial
+{
+    public class NameModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
